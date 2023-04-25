25228d3ea5cd0e5a9f5e282f8e3c1ff4db843a45
Cal <obinnaanizoba5@gmail.com>
2023-04-26 00:34:59 +0800

    refractoring code

--- shopping-cart/src/components/CartPage/productItem.tsx
+++ shopping-cart/src/components/CartPage/productItem.tsx
@@ -1,89 +1,103 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { CartItemsContext } from "../Contexts/cartItemsContext";
 import { ProductItemContext } from "../Contexts/productItemContext";
-
+import { emptyProductItemCart } from "./emptyProductCart";
+import { Decrement } from "./decrement";
+import { AddProductItemToCart } from "./addItemToCart";
+import { Increment } from "./increment";
 export const ProductItem = () => {
 
-    const { productItem, setProductItem } = useContext(ProductItemContext);
+    const { productItem } = useContext(ProductItemContext);
 
-    const {setCartItems} = useContext(CartItemsContext);
+    const { cartItems, setCartItems } = useContext(CartItemsContext);
 
-    // type total = number;
-    // type initialNumber = number;
-    // useEffect(()=>{
-        // setProductItem((eachProduct)=> eachProduct.map((eachProductItem)=> ({...eachProductItem,  amount: eachProductItem.quantity * eachProductItem.price })))
-    // },[]);
+    // const increment = (item: {
+    //     id: number,
+    //     title: string,
+    //     price: number,
+    //     description: string,
+    //     category: string,
+    //     image: string,
+    //     rating: {
+    //         rate: string,
+    //         count: number
+    //     },
+    //     amount: number,
+    //     quantity: number
+    // }) => {
 
-    const increment = (item: {
-        id: number,
-        title: string,
-        price: number,
-        description: string,
-        category: string,
-        image: string,
-        rating: {
-            rate: string,
-            count: number
-        },
-        amount: number,
-        quantity: number
-    }) => {
+    //     //increment quantity by 1
+    //     setProductItem((eachProduct) => eachProduct.map((eachProductItem) => eachProductItem.id === item.id ? { ...eachProductItem, quantity: eachProductItem.quantity + 1 } : eachProductItem));
+    //     //update the amount by getting the updated quantity to multiply price
+    //     setProductItem((eachProduct) => eachProduct.map((eachProductItem) => eachProductItem.id === item.id ? { ...eachProductItem, amount: eachProductItem.quantity * eachProductItem.price } : eachProductItem));
 
-        //increment quantity by 1
-        setProductItem((eachProduct) => eachProduct.map((eachProductItem) => eachProductItem.id === item.id ? { ...eachProductItem, quantity: eachProductItem.quantity + 1} : eachProductItem));
-        //update the amount by getting the updated quantity to multiply price
-        setProductItem((eachProduct) => eachProduct.map((eachProductItem) => eachProductItem.id === item.id ? { ...eachProductItem, amount: eachProductItem.quantity * eachProductItem.price } : eachProductItem));
-    }
+    // }
 
-    const decrement = (item: {
-        id: number,
-        title: string,
-        price: number,
-        description: string,
-        category: string,
-        image: string,
-        rating: {
-            rate: string,
-            count: number
-        },
-        amount: number,
-        quantity: number
-    }) => {
-        if (item.quantity === 1) {
-            removeProductItem(item);
-        } else {
-        //decrement quantity by 1
-        setProductItem((eachProduct) => eachProduct.map((eachProductItem) => eachProductItem.id === item.id ? { ...eachProductItem, quantity: eachProductItem.quantity - 1} : eachProductItem));
-        //update the amount by getting the updated quantity to multiply price
-        setProductItem((eachProduct) => eachProduct.map((eachProductItem) => eachProductItem.id === item.id ? { ...eachProductItem, amount: eachProductItem.quantity * eachProductItem.price } : eachProductItem));
-        }
-    }
+    // const decrement = (item: {
+    //     id: number,
+    //     title: string,
+    //     price: number,
+    //     description: string,
+    //     category: string,
+    //     image: string,
+    //     rating: {
+    //         rate: string,
+    //         count: number
+    //     },
+    //     amount: number,
+    //     quantity: number
+    // }) => {
+    //     if (item.quantity === 1) {
+    //         removeProductItem(item);
+    //     } else {
+    //         //decrement quantity by 1
+    //         setProductItem((eachProduct) => eachProduct.map((eachProductItem) => eachProductItem.id === item.id ? { ...eachProductItem, quantity: eachProductItem.quantity - 1 } : eachProductItem));
+    //         //update the amount by getting the updated quantity to multiply price
+    //         setProductItem((eachProduct) => eachProduct.map((eachProductItem) => eachProductItem.id === item.id ? { ...eachProductItem, amount: eachProductItem.quantity * eachProductItem.price } : eachProductItem));
+    //     }
+    // }
 
-    const removeProductItem = (item: {
-        id: number,
-        title: string,
-        price: number,
-        description: string,
-        category: string,
-        image: string,
-        rating: {
-            rate: string,
-            count: number
-        },
-        amount: number
-    }) => {
-        setProductItem((eachProductItem) => (
-            eachProductItem.filter((eachProductItem) => eachProductItem.id !== item.id)
-        ));
-    }
+    // const removeProductItem = (item: {
+    //     id: number,
+    //     title: string,
+    //     price: number,
+    //     description: string,
+    //     category: string,
+    //     image: string,
+    //     rating: {
+    //         rate: string,
+    //         count: number
+    //     },
+    //     amount: number
+    // }) => {
+    //     setProductItem((eachProductItem) => (
+    //         eachProductItem.filter((eachProductItem) => eachProductItem.id !== item.id)
+    //     ));
+    // }
 
-    const emptyProductItemCart = () => {
-        return (
-            <div>
-                <p>Cart selected is empty, go back to shopping</p>
-            </div>)
-    }
+    // const addProductItemToCart = (item: {
+    //     id: number,
+    //     title: string,
+    //     price: number,
+    //     description: string,
+    //     category: string,
+    //     image: string,
+    //     rating: {
+    //         rate: string,
+    //         count: number
+    //     },
+    //     amount: number,
+    //     quantity: number
+    // }, quantity: number, amount: number) => {
+    //     if (cartItems.some((cartItem) => cartItem.id === item.id)) {
+    //         setCartItems((cartItems) => cartItems.map((eachCartItem) => eachCartItem.id === item.id ? { ...eachCartItem, quantity: eachCartItem.quantity + quantity, amount: eachCartItem.amount + amount } : eachCartItem));
+    //     } else {
+    //         setCartItems((cartItems) => [...cartItems, item])
+    //     }
+    // }
+
+
 
     return (
         productItem.length === 1 ? (
@@ -107,28 +121,45 @@ export const ProductItem = () => {
                             <div id="title-container">
                                 <p>{item.title}</p>
                             </div>
-                            <div id="pqt-container" className="flex gap-10">
-                                <div id="price-container">
-                                    <p> ${item.price}</p>
-                                </div>
-                                <div className="flex justify-center items-center gap-2">
-                                    <div id="decrement-button">
-                                        <button type="button" onClick={() => decrement(item)}>-</button>
+                            <div id="pqt-add-to-cart-btn-holder" className="flex flex-col gap-2">
+                                <div id="pqt-container" className="flex gap-10">
+                                    <div id="price-container">
+                                        <p> ${item.price}</p>
                                     </div>
-                                    <div>{item.quantity}</div>
-                                    <div id="increment-button">
-                                        <button type="button" onClick={() => increment(item)}>+</button>
+                                    <div className="flex justify-center items-center gap-2">
+                                        <div id="decrement-button">
+                                         <Decrement item={item}/>
+                                        </div>
+                                        <div>{item.quantity}</div>
+                                        <Increment item ={item}/>
                                     </div>
+                                    <div id="total-price-container">{item.amount}</div>
                                 </div>
-                                <div id="total-price-container">{item.amount}</div>
+                                <div id="add-to-cart-btn" className="flex justify-end">
+                                    <Link to="/Shop"><button onClick={() => { AddProductItemToCart(item, item.quantity, item.amount) }}>Add to Cart</button></Link>
+                                </div>
                             </div>
                         </div>
                     ))}
                 </div>
-                <div id="add-to-cart-btn" className="flex justify-end m-3">
-                    {/* <button onClick={()=>{setCartItems(productItem)}}>Add to Cart</button> */}
-                </div>
             </div>
         ) : emptyProductItemCart())
 
+}
+
+export interface IncreDecrementProps {
+    item: {
+        id: number,
+        title: string,
+        price: number,
+        description: string,
+        category: string,
+        image: string,
+        rating: {
+            rate: string,
+            count: number
+        },
+        amount: number,
+        quantity: number
+    }
 }
